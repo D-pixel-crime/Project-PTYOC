a8083e19a441bcc9f2c62936544235cc9e1e646a
D-pixel-crime <dpk4383@gmail.com>
2024-04-06 19:19:00 +0530

    custom hooks

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,28 @@
-const Meeting = ({ params }: { params: { id: String } }) => {
-  return <div>Meeting: {params.id}</div>;
+"use client";
+
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/customHooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isAudioVideoSetupDone, setIsAudioVideoSetupDone] = useState(false);
+  const { currentLiveCall, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="w-full h-screen">
+      <StreamCall call={currentLiveCall}>
+        <StreamTheme>
+          {!isAudioVideoSetupDone ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 export default Meeting;
