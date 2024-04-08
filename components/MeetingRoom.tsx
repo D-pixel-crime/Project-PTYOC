b1f40ce6e0b1a3c1c1911c52454fc5ce26fe7a9b
D-pixel-crime <dpk4383@gmail.com>
2024-04-08 09:15:11 +0530

    Meeting Layout start

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -5,6 +5,15 @@ import {
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
 import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
 
 type CallLayoutOptions = "grid" | "focus-left" | "focus-right";
 
@@ -39,8 +48,25 @@ const MeetingRoom = () => {
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
-      <div className="flex-center fixed bottom-0 gap-5">
+      <div className="flex-center w-full fixed bottom-0 gap-5 mx-4">
         <CallControls />
+
+        <DropdownMenu>
+          <div className="flex-center">
+            <DropdownMenuTrigger className="rounded-full hover:bg-white hover:text-black">
+              <LayoutList className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Layout</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {["grid", "focus-left", "focus-right"].map((element, index) => (
+              <DropdownMenuItem className="cursor-pointer" key={index}>
+                {element}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </section>
   );
